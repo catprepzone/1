4521d8cbfa7fbbf2de84f1c79eeac4a28d8fd5bc
catprepzone <siddharthgodhani0@gmail.com>
2025-12-28 17:07:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// main.js
+fetch("../layout.html")
+  .then(res => res.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+
+    // Inject Navbar
+    document.getElementById("navbar").innerHTML =
+      doc.getElementById("site-navbar").innerHTML;
+
+    // Inject Footer
+    document.getElementById("footer").innerHTML =
+      doc.getElementById("site-footer").innerHTML;
+  });
